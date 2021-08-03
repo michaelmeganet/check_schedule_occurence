803fa78d301f547acbc347340d097c4d8f43f02c
Thraxelon <claudio.christyo@gmail.com>
2021-08-03 18:06:32 +0700

    Added Legacy insert in batch insertion

--- batchinsert_schedule_date.php
+++ batchinsert_schedule_date.php
@@ -1,6 +1,8 @@
 <?php
 include_once 'class/dbh.inc.php';
 include_once 'class/variables.inc.php';
+include_once 'class/legacy_dbh.inc.php';
+include_once 'class/legacy_variables.inc.php';
 include_once 'class/phhdate.inc.php';
 
 //to check debug $_POST
@@ -15,91 +17,99 @@ function getPeriodList() {
     $setofPeriod = $objPeriod->generatePeriod3();
     return $setofPeriod;
 }
-function createSchedulingData($schtab,$ord_dataset) {
+
+function createSchedulingData($schtab, $ord_dataset) {
 
     foreach ($ord_dataset as $key => $value) {
-            ${$key} = $value;
-            // echo "$key : $value \n"."<br>";
-        }
-        if ($bid == 1) {
-            $jlfor = 'CJ';
-        } elseif ($bid == 2) {
-            $jlfor = 'SB';
-        } else {
-            $jlfor = '';
-        }
-        echo " \$completion_date = $completion_date<br>";
-        $detectStr = substr($completion_date,2,1);
-        if ($detectStr == '-') {
-        	# code...
-			$date = "";
-			$date = date_create_from_format('d-m-y', $completion_date);
-			$newformat = date_format($date, 'Y-m-d');
-			echo "\$newformat = $newformat <br>";
-			echo "<br>";
-			$completion_date = $newformat;
-			echo "\$completion_date = $completion_date <br>";
-        }
+        ${$key} = $value;
+        // echo "$key : $value \n"."<br>";
+    }
+    if ($bid == 1) {
+        $jlfor = 'CJ';
+    } elseif ($bid == 2) {
+        $jlfor = 'SB';
+    } else {
+        $jlfor = '';
+    }
+    echo " \$completion_date = $completion_date<br>";
+    $detectStr = substr($completion_date, 2, 1);
+    if ($detectStr == '-') {
+        # code...
+        $date = "";
+        $date = date_create_from_format('d-m-y', $completion_date);
+        $newformat = date_format($date, 'Y-m-d');
+        echo "\$newformat = $newformat <br>";
+        echo "<br>";
+        $completion_date = $newformat;
+        echo "\$completion_date = $completion_date <br>";
+    }
 
-        $Insert_Array = array();
-        $Insert_Array = array(
-            'bid' => $bid,
-            'qid' => $qid,
-            'quono' => $quono,
-            'company' => $company,
-            'status' => $cusstatus,
-            'cid' => $cid,
-            'noposition' => $noposition,
-            'quantity' => $quantity,
-            'grade' => $grade,
-            'mdt' => $mdt,
-            'mdw' => $mdw,
-            'mdl' => $mdl,
-            'fdt' => $fdt,
-            'fdw' => $fdw,
-            'fdl' => $fdl,
-            'process' => $process,
-            'cncmach' => $cncmach,
-            'aid_cus' => $aid_cus,
-            'date_issue' => $date_issue,
-            'source' => $source,
-            'cuttingtype' => $cuttingtype,
-            'custoolcode' => $custoolcode,
-            'completion_date' => $completion_date,
-            'runningno' => $runningno,
-            'jlfor' => $jlfor,
-            'jobno' => $jobno,
-            'ivdate' => $ivdate,
-            'operation' => $operation
-        );
+    $Insert_Array = array();
+    $Insert_Array = array(
+        'bid' => $bid,
+        'qid' => $qid,
+        'quono' => $quono,
+        'company' => $company,
+        'status' => $cusstatus,
+        'cid' => $cid,
+        'noposition' => $noposition,
+        'quantity' => $quantity,
+        'grade' => $grade,
+        'mdt' => $mdt,
+        'mdw' => $mdw,
+        'mdl' => $mdl,
+        'fdt' => $fdt,
+        'fdw' => $fdw,
+        'fdl' => $fdl,
+        'process' => $process,
+        'cncmach' => $cncmach,
+        'aid_cus' => $aid_cus,
+        'date_issue' => $date_issue,
+        'source' => $source,
+        'cuttingtype' => $cuttingtype,
+        'custoolcode' => $custoolcode,
+        'completion_date' => $completion_date,
+        'runningno' => $runningno,
+        'jlfor' => $jlfor,
+        'jobno' => $jobno,
+        'ivdate' => $ivdate,
+        'operation' => $operation
+    );
 
-        $qrins = "INSERT INTO $schtab SET ";
-        $qrins_debug = "INSERT INTO $schtab SET ";
-        $arrCnt = count($Insert_Array);
-        $cnt = 0;
-        foreach ($Insert_Array as $key => $val) {
-            $cnt++;
-            $qrins .= " $key =:$key ";
-            $qrins_debug .= " $key = '$val' ";
-            if ($cnt != $arrCnt) {
-                $qrins .= " , ";
-                $qrins_debug .= " , ";
-            }
+    $qrins = "INSERT INTO $schtab SET ";
+    $qrins_debug = "INSERT INTO $schtab SET ";
+    $arrCnt = count($Insert_Array);
+    $cnt = 0;
+    foreach ($Insert_Array as $key => $val) {
+        $cnt++;
+        $qrins .= " $key =:$key ";
+        $qrins_debug .= " $key = '$val' ";
+        if ($cnt != $arrCnt) {
+            $qrins .= " , ";
+            $qrins_debug .= " , ";
         }
+    }
 
 //            echo "<br><br>\$qrins = $qrins <br><br>";
 //            echo "<br><br>\$qrins_debug= $$qrins_debug <br><br>";
-            echo "<br>$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$<br>";
+    echo "<br>$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$<br>";
+    echo "===DEBUG LOG QR = $qrins_debug <br>";
 
-        $objSQLlog = new SQLBINDPARAM($qrins, $Insert_Array);
-        $insResult = $objSQLlog->InsertData2();
-            echo "===DEBUG LOG QR = $qrins_debug <br>";
-            echo "+++===LOG RESULT = $insResult<br>";
-    
-    return $insResult;
+    $objSQLlog = new SQLBINDPARAM($qrins, $Insert_Array);
+    $insResult = $objSQLlog->InsertData2();
+    echo "+++===LOG RESULT (Target : Localhost) = $insResult<br>";
+
+    echo "<br>--------------------------------------------------------------------------------------------------<br>";
+
+    $objLegacySQLlog = new legacySQLBINDPARAM($qrins, $Insert_Array);
+    $insLegacyResult = $objLegacySQLlog->InsertData2();
+    echo "===DEBUG LOG QR = $qrins_debug <br>";
+    echo "+++===LEGACY LOG RESULT (Target : 192.168.102.11) = $insLegacyResult<br>";
 
     echo "<br>##########################################################################################<br>";
-    }
+
+    return $insResult;
+}
 
 $periodList = getPeriodList();
 
@@ -107,7 +117,7 @@ if (isset($_POST['period'])) {
     $period = $_POST['period'];
     $com = $_POST['com'];
     $com_sml = strtolower($com);
-    $com_cap = strtoupper($com);    
+    $com_cap = strtoupper($com);
     $ordtab = "orderlist_{$com_sml}_$period";
     $schtab = "production_scheduling_$period";
 
@@ -187,11 +197,11 @@ function check_schRecord($schtab, $qno, $cid, $bid, $runno, $nopos) {
 <!DOCTYPE html>
 <html lang="en">
 
-    <?php include "header.php"; ?>
+<?php include "header.php"; ?>
 
     <body>
 
-        <?php #include"navmenu.php";    ?>
+<?php #include"navmenu.php";     ?>
 
         <div class="container">
 
@@ -276,55 +286,54 @@ function check_schRecord($schtab, $qno, $cid, $bid, $runno, $nopos) {
                                             </tr>
                                         </thead>
                                         <tbody> -->
-                                            <?php
-                                            foreach ($ordvsch as $ordvsch_row) {
-                                                // echo "<tr>";
-                                                foreach ($ordvsch_row as $index => $val) {
+                                    <?php
+                                    foreach ($ordvsch as $ordvsch_row) {
+                                        // echo "<tr>";
+                                        foreach ($ordvsch_row as $index => $val) {
 
-                                                    // echo "<td>$val</td>";
-                                                }
-                                                if ($ordvsch_row['sch_stat'] == 'no') {
-                                                    echo "
-                                                    <td>
-                                                        <a target='_blank' href = 'createschedulingrecord.php?period=$period&qno={$ordvsch_row['qno']}&com={$ordvsch_row['com']}&cid={$ordvsch_row['cid']}&bid={$ordvsch_row['bid']}&runno={$ordvsch_row['runno']}&nopos={$ordvsch_row['noposition']}'
-                                                            class='btn btn-warning btn-sm disabled'>Generate Scheduling Data</a>
-                                                    </td>";
-                                                  
-                                                    $qno = $ordvsch_row['qno'];
-                                                    $bid = $ordvsch_row['bid'];
-                                                    $cid = $ordvsch_row['cid'];
-                                                    $runno = $ordvsch_row['runno'];
-                                                    $nopos = $ordvsch_row['noposition'];
+                                            // echo "<td>$val</td>";
+                                        }
+                                        if ($ordvsch_row['sch_stat'] == 'no') {
+//                                            echo "
+////                                                    <td>
+////                                                        <a target='_blank' href = 'createschedulingrecord.php?period=$period&qno={$ordvsch_row['qno']}&com={$ordvsch_row['com']}&cid={$ordvsch_row['cid']}&bid={$ordvsch_row['bid']}&runno={$ordvsch_row['runno']}&nopos={$ordvsch_row['noposition']}'
+////                                                            class='btn btn-warning btn-sm disabled'>Generate Scheduling Data</a>
+////                                                    </td>";
 
-													$ordtab = "orderlist_pst_$period";
-													$schtab = "production_scheduling_$period";
+                                            $qno = $ordvsch_row['qno'];
+                                            $bid = $ordvsch_row['bid'];
+                                            $cid = $ordvsch_row['cid'];
+                                            $runno = $ordvsch_row['runno'];
+                                            $nopos = $ordvsch_row['noposition'];
 
-													echo "<strong>GENERATING SCHEDULING RECORD FOR $qno</strong><br>";
-													echo "==== Fetch Orderlist record ====<br>";
-													$qrord = "SELECT * FROM $ordtab WHERE quono = '$qno' AND bid = $bid AND cid = $cid AND runningno = '$runno' AND noposition = '$nopos'";
-													$objSQLord = new SQL($qrord);
-													echo "\$qrord = $qrord<br";
-													$ord_dataset = $objSQLord->getResultOneRowArray();
-													if (empty($ord_dataset)) {
-													    echo "Failed to fetch data for quono =$qno; cid = $cid; bid=$bid; runningno = $runno; noposition= $nopos";
-													    exit();
-													} else {
-													    print_r($ord_dataset);
-													    echo "<br><br>";
-													    $createResult = createSchedulingData($schtab, $ord_dataset);
-													    //$createResult = "The Create scheduling data is run here ";
-													    echo "<strong>Result : $createResult</strong><br>";
-													    
-													}
-													                                                }else{
-                                                    // echo "<td>&nbsp;</td>";
-                                                }
-                                                ?>
-                                                <?php
-                                                // echo"</tr>";
+                                            $ordtab = "orderlist_pst_$period";
+                                            $schtab = "production_scheduling_$period";
+
+                                            echo "<strong>GENERATING SCHEDULING RECORD FOR $qno</strong><br>";
+                                            echo "==== Fetch Orderlist record ====<br>";
+                                            $qrord = "SELECT * FROM $ordtab WHERE quono = '$qno' AND bid = $bid AND cid = $cid AND runningno = '$runno' AND noposition = '$nopos'";
+                                            $objSQLord = new SQL($qrord);
+                                            echo "\$qrord = $qrord<br";
+                                            $ord_dataset = $objSQLord->getResultOneRowArray();
+                                            if (empty($ord_dataset)) {
+                                                echo "Failed to fetch data for quono =$qno; cid = $cid; bid=$bid; runningno = $runno; noposition= $nopos";
+                                                exit();
+                                            } else {
+                                                print_r($ord_dataset);
+                                                echo "<br><br>";
+                                                $createResult = createSchedulingData($schtab, $ord_dataset);
+                                                //$createResult = "The Create scheduling data is run here ";
+                                                echo "<strong>Result : $createResult</strong><br>";
                                             }
-                                            ?>
-                                        </tbody>
+                                        } else {
+                                            // echo "<td>&nbsp;</td>";
+                                        }
+                                        ?>
+                                        <?php
+                                        // echo"</tr>";
+                                    }
+                                    ?>
+                                    </tbody>
                                     </table>
                                 </div>
                             </div>
